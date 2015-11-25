f7d4474ce668e0878c65a538ada5d11353693fd9
mincong-h <mincong.h@gmail.com>
2015-11-25 17:32:48 +0100

    TP 3 V0.1

---
+++ TestMapping/src/main/java/fr/esigelec/Hello.java
@@ -0,0 +1,43 @@
+package fr.esigelec;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ * 
+ * @author m.huang.11, f.chouakria.13
+ */
+@WebServlet(urlPatterns = {"*.php", "*.do"})
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.getWriter().append("Bonjour venant de ").append(request.getRequestURI());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.getWriter().append("Bonjour(doPost)");
+	}
+
+}
